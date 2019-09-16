adcab25f27527cfb2a5607483666fbb41c36bf21
Francisco Mcallister Ortiz <fj.mac@uniandes.edu.co>
2019-09-16 16:43:09 -0500

    inicial

---
+++ front/build/precache-manifest.42aa48a340498642c6ee2d3018ca2004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557c430d950e96e04eda2715b9a9660d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e549ed089cba5104ddc3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "516f974f493561d326e6",
+    "url": "/static/js/2.73c2a480.chunk.js"
+  },
+  {
+    "revision": "e549ed089cba5104ddc3",
+    "url": "/static/js/main.1e266583.chunk.js"
+  },
+  {
+    "revision": "9ba826fd182ef2cb5489",
+    "url": "/static/js/runtime~main.cef8c144.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
